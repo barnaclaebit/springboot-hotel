5bd1fb492e522a204000cea528eb529d927b85d3
halcyon <Alan1999gustavo99@gmail.com>
2023-07-16 02:29:12 -0300

    project initialized

---
+++ src/test/java/com/barnaclaebit/project/ApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barnaclaebit.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
